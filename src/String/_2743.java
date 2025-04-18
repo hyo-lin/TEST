23d0461f6cf2a51ae9c85233bbc3b330c3695532
LHL <lin1137001@gmail.com>
2025-04-18 23:48:14 +0900

    2743

---
+++ src/String/_2743.java
@@ -0,0 +1,20 @@
+package String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class _2743  {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+        String b= bf.readLine();
+        int n=b.length();
+        System.out.println(n);
+
+
+
+    }
+
+
+}
