ecffa51f8ed28c2b260d1d94fdd739c2bbccd0b2
LHL <lin1137001@gmail.com>
2025-04-20 22:39:02 +0900

    11654

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,10 @@ import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main  {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
+
+
+
 
 
       }
---
+++ src/String/_11654.java
@@ -0,0 +1,21 @@
+package String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class _11654  {
+    public static void main(String[] args) throws IOException{
+        BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+        String n=bf.readLine();
+        int f=(int)n.charAt(0);
+        System.out.println(f);
+
+
+
+
+    }
+
+
+}
