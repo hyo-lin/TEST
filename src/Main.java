92c2e1f4103305c215f314d638c8c78004105586
LHL <lin1137001@gmail.com>
2025-04-25 00:00:46 +0900

    10809

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
-public class Main  {
+public class Main {
     public static void main(String[] args) throws IOException{
+
+
 
 
       }
---
+++ src/String/_10809.java
@@ -0,0 +1,25 @@
+package String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class _10809 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+        String str=bf.readLine();
+
+        StringBuilder sb=new StringBuilder();
+        for(char c='a'; c<='z'; c++){
+            sb.append(str.indexOf(c)+ " ");
+        }
+        System.out.println(sb);
+
+
+
+    }
+
+
+}
