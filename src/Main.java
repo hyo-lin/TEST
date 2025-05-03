7c2c3ee74ac61d4bdc4b2041225ec47b7ef0f661
LHL <lin1137001@gmail.com>
2025-05-03 23:58:03 +0900

    1152

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
-
+    public static void main(String[] args) {
 
       }
 
---
+++ src/String/_1152.java
@@ -0,0 +1,19 @@
+package String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class _1152 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(bf.readLine()," ");
+        System.out.println(st.countTokens());
+
+    }
+
+
+}
