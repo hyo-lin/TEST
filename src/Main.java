e8cd797f86d84b1ede745e5cf745ef73ec84f219
hyo-lin <lin1137001@gmail.com>
2025-05-29 23:44:34 +0900

    2908

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,8 @@ import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
 
       }
 
