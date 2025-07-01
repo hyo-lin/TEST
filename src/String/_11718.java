8b1c9517bc312d0edb572d12dabcd86421fd3adc
hyo-lin <lin1137001@gmail.com>
2025-07-01 22:39:24 +0900

    11718

--- src/String/_11718.java
+++ src/String/_11718.java
@@ -1,3 +1,5 @@
+package String;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -5,12 +7,17 @@ import java.io.InputStreamReader;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
-public class Main {
+public class _11718 {
     public static void main(String[] args) throws IOException {
 
+        BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+        String str;
 
-
-
+        while((str=bf.readLine())!=null){
+            System.out.println(str);
+        }
+        bf.close();
     }
 
-    }
+
+}
