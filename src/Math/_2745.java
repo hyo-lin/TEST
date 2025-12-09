634936ba8e5981d1902de26e9ae40430e4edfd97
hyo-lin <lin1137001@gmail.com>
2025-12-09 23:59:21 +0900

    2745

--- src/Math/_2745.java
+++ src/Math/_2745.java
@@ -10,37 +10,19 @@ public class _2745 {
         // 첫째 줄에서 N(B진법 수)과 B(진법)를 공백으로 구분하여 입력 받음
         String N = scanner.next();
         int B = scanner.nextInt();
+        int result=0;
 
-        // 10진수 결과를 저장할 변수 (결과가 10억 이하이므로 int 또는 long 사용 가능)
-        long decimalValue = 0;
+        for(int i=0; i<N.length(); i++){
+            char c=N.charAt(i);
+            int value;
 
-        // B의 거듭제곱(B^0, B^1, B^2, ...)을 저장할 변수. 가장 낮은 자릿수부터 시작하므로 1로 초기화
-        long powerOfB = 1;
+            if(c>='0' && c<='9') value = c - '0';
+            else value=c-'A'+10;
 
-        // B진법 수 N의 문자열을 가장 낮은 자릿수(오른쪽 끝)부터 순회
-        for (int i = N.length() - 1; i >= 0; i--) {
-            char digit = N.charAt(i);
-            int digitValue = 0; // 현재 자릿수의 10진수 값 (0 ~ 35)
-
-            // 1. 자릿값(digit)을 10진수 값으로 변환
-            if (digit >= '0' && digit <= '9') {
-                // 0부터 9 사이의 숫자
-                digitValue = digit - '0';
-            } else if (digit >= 'A' && digit <= 'Z') {
-                // A부터 Z 사이의 알파벳 (A=10, B=11, ...)
-                digitValue = (digit - 'A') + 10;
-            }
-
-            // 2. 10진수 결과에 '자릿값 * B의 거듭제곱'을 더함
-            decimalValue += digitValue * powerOfB;
-
-            // 3. 다음 자릿수를 위해 B의 거듭제곱을 업데이트
-            // (다음 자릿수는 현재 자릿값보다 B배 더 큰 자릿값을 가짐)
-            powerOfB *= B;
+            result=result*B+value;
         }
+        System.out.println(result);
 
-        // 결과 출력
-        System.out.println(decimalValue);
 
         scanner.close();
     }
