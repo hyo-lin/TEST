06b21449e5807f2b04abdb21f4216db390de6f5d
hyo-lin <lin1137001@gmail.com>
2025-12-12 23:57:21 +0900

    11005

---
+++ src/Math/_11005.java
@@ -0,0 +1,38 @@
+package Math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _11005 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        int N=Integer.parseInt(st.nextToken());
+        int B=Integer.parseInt(st.nextToken());
+
+        StringBuilder sb=new StringBuilder();
+        if(N==0){
+            sb.append('0');
+        } else{
+            while(N>0){
+                int rest=N%B;
+                char c;
+                if(rest<10){
+                    c=(char)('0'+rest);
+                }else{
+                    c=(char)('A'+rest-10);
+                }
+                sb.append(c);
+                N/=B;
+            }
+        }
+        System.out.println(sb.reverse().toString());
+
+        bf.close();
+
+    }
+}
