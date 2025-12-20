6e726c6c810ab6af4bc44a11f5e7187463bdad70
hyo-lin <lin1137001@gmail.com>
2025-12-20 23:55:27 +0900

    2292

---
+++ src/Math/_2292.java
@@ -0,0 +1,18 @@
+package Math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _2292 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(bf.readLine());
+        for(int p=0; p<n; p++){
+            if(p*(p+1)<n*2){
+
+            }
+
+        }
+    }
+}
