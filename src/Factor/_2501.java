6d7422115e2937219ce3280f13f648c845785eae
hyo-lin <lin1137001@gmail.com>
2025-12-23 23:58:48 +0900

    2501

---
+++ src/Factor/_2501.java
@@ -0,0 +1,19 @@
+package Factor;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _2501 {
+    public static void main(String[] args)throws IOException{
+        BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(bf.readLine());
+        int n=Integer.parseInt(st.nextToken());
+     for(int i=1; i<n; i++){
+         if(n%i==0){
+
+         }
+     }
+    }
+}
