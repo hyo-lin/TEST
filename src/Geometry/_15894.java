3922d5dfe6060402986ba945095b68073edd74ae
hyo-lin <lin1137001@gmail.com>
2025-12-31 23:51:16 +0900

    15894

---
+++ src/Geometry/_15894.java
@@ -0,0 +1,13 @@
+package Geometry;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _15894 {
+    public static void main(String[] args)throws IOException{
+        BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+        long n=Integer.parseInt(bf.readLine());
+        System.out.println(n*4);
+    }
+}
