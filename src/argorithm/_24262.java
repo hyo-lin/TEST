7ceb6f15525e418d8f6c48263d5b5240cb9aa0d5
hyo-lin <lin1137001@gmail.com>
2026-01-02 14:19:33 +0900

    24262

--- src/argorithm/_24262.java
+++ src/argorithm/_24262.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 
 public class _24262 {
     public static void main(String[] args)throws IOException{
+System.out.println(1);
+        System.out.println(0);
 
     }
 }
