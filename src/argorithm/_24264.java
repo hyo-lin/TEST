e5929e147836532dfa6ffe4cfb1217c815ba69b0
hyo-lin <lin1137001@gmail.com>
2026-01-02 15:03:18 +0900

    24264

---
+++ src/argorithm/_24264.java
@@ -0,0 +1,14 @@
+package argorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _24264 {
+    public static void main(String[] args)throws IOException{
+        BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+        long n=Integer.parseInt(bf.readLine());
+        System.out.println(n*n);
+        System.out.println(2);
+    }
+}
