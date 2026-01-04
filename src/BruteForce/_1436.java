a3f70bf0bb0a556c5cf0ecf7770623e967640212
hyo-lin <lin1137001@gmail.com>
2026-01-04 23:23:23 +0900

    1436

---
+++ src/BruteForce/_1436.java
@@ -0,0 +1,22 @@
+package BruteForce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _1436 {
+    public static void main(String[] args)throws IOException{
+        BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+        int N=Integer.parseInt(bf.readLine());
+        int count=1;
+        int num=666;
+
+        while(count!=N){
+            num++;
+            if(String.valueOf(num).contains("666")){
+                count++;
+            }
+        }
+        System.out.println(num);
+    }
+}
