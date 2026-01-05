6000f147abbc9649f6ea6a7beffd70d5015a8527
hyo-lin <lin1137001@gmail.com>
2026-01-05 23:59:05 +0900

    2750

---
+++ src/Sort/_2750.java
@@ -0,0 +1,22 @@
+package Sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class _2750 {
+    public static void main(String[] args)throws IOException{
+        BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+        int N=Integer.parseInt(bf.readLine());
+        int[] arr=new int[N];
+        for(int i=0; i<N; i++){
+            arr[i]=Integer.parseInt(bf.readLine());
+        }
+        Arrays.sort(arr);
+
+        for(int i=0; i<N; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
