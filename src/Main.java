89f8de714b5838eef93217f5190f760644e96448
hyo-lin <lin1137001@gmail.com>
2026-01-09 16:41:17 +0900

    17103

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,30 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Scanner;
+import java.io.*;
+import java.sql.SQLOutput;
 import java.util.StringTokenizer;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        int n=Integer.parseInt(st.nextToken());
+        int p=Integer.parseInt(st.nextToken());
 
+        int gcd=getGCD(n,p);
 
-
+        System.out.println(gcd);
+        System.out.println(((n*p)/gcd));
 
     }
 
+    private static int getGCD(int n, int p){
+        if(n%p==0) return p;
+        return getGCD(p,n%p);
     }
+
+    }
+
+
+
+
+
