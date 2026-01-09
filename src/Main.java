f57cc605980efc56e2a041158cfd570023ca9825
hyo-lin <lin1137001@gmail.com>
2026-01-09 16:42:19 +0900

    main

--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,7 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st=new StringTokenizer(br.readLine());
-        int n=Integer.parseInt(st.nextToken());
-        int p=Integer.parseInt(st.nextToken());
 
-        int gcd=getGCD(n,p);
-
-        System.out.println(gcd);
-        System.out.println(((n*p)/gcd));
-
-    }
-
-    private static int getGCD(int n, int p){
-        if(n%p==0) return p;
-        return getGCD(p,n%p);
     }
 
     }
