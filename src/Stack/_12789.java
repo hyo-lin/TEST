13ee323e9e20948c2cc278989990a8f4e1cf8b2f
hyo-lin <lin1137001@gmail.com>
2026-01-11 23:52:49 +0900

    12789

---
+++ src/Stack/_12789.java
@@ -0,0 +1,20 @@
+package Stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _12789 {
+    public static void main(String[] args)throws IOException{
+        BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+        int N=Integer.parseInt(bf.readLine());
+
+        for(int i=0; i<N; i++){
+            StringTokenizer st=new StringTokenizer(bf.readLine());
+            int s=Integer.parseInt(st.nextToken());
+
+
+        }
+    }
+}
