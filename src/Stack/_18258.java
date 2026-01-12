8b83b6005ea7f2b41669375acc24afe2ce1e7ded
hyo-lin <lin1137001@gmail.com>
2026-01-13 01:23:12 +0900

    18258

--- src/Stack/_18258.java
+++ src/Stack/_18258.java
@@ -5,19 +5,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class _18258 {
     public static void main(String[] args)throws IOException{
         BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st=new StringTokenizer(bf.readLine());
         int N=Integer.parseInt(bf.readLine());
         Deque<Integer> q = new ArrayDeque<>();
 
         for(int i=0; i<N; i++){
-            st=new StringTokenizer(bf.readLine());
+            StringTokenizer st=new StringTokenizer(bf.readLine());
             String command=st.nextToken();
 
             switch(command){
@@ -30,15 +28,18 @@ public class _18258 {
                     else sb.append(q.poll()).append("\n");
                     break;
                 case "size":
+                    sb.append(q.size()).append("\n");
+                    break;
+                case "empty":
                     if(q.isEmpty()) sb.append("1").append("\n");
                     else sb.append("0").append("\n");
                     break;
                 case "front":
-                    if(q.isEmpty()) sb.append("1").append("\n");
+                    if(q.isEmpty()) sb.append("-1").append("\n");
                     else sb.append(q.peek()).append("\n");
                     break;
                 case "back":
-                    if(q.isEmpty()) sb.append("1").append("\n");
+                    if(q.isEmpty()) sb.append("-1").append("\n");
                     else sb.append(q.peekLast()).append("\n");
                     break;
             }
