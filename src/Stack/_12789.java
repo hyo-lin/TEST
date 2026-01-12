fa0d5ef8ee18bc459feab8123bab66ffb920800a
hyo-lin <lin1137001@gmail.com>
2026-01-12 23:55:19 +0900

    12789

--- src/Stack/_12789.java
+++ src/Stack/_12789.java
@@ -3,18 +3,39 @@ package Stack;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class _12789 {
     public static void main(String[] args)throws IOException{
         BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
-        int N=Integer.parseInt(bf.readLine());
+        bf.readLine();
+        Stack<Integer> stack=new Stack<>();
+        StringTokenizer st=new StringTokenizer(bf.readLine());
+        int target=1;
 
-        for(int i=0; i<N; i++){
-            StringTokenizer st=new StringTokenizer(bf.readLine());
-            int s=Integer.parseInt(st.nextToken());
+        while(st.hasMoreTokens()){
+            int current=Integer.parseInt(st.nextToken());
+
+            if(current==target){
+                target++;
+            }
+            else{
+                stack.push(current);
+            }
+            while(!stack.isEmpty() && stack.peek()==target){
+                stack.pop();
+                target++;
+            }
 
 
         }
+        if(stack.isEmpty()){
+            System.out.println("Nice");
+
+        } else{
+            System.out.println("Sad");
+        }
+
     }
 }
