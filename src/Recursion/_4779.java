3dfc6408cff07c37709a032b9d65284371f53188
hyo-lin <lin1137001@gmail.com>
2026-01-20 23:57:31 +0900

    4779

--- src/Recursion/_4779.java
+++ src/Recursion/_4779.java
@@ -5,19 +5,36 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class _4779 {
+    static StringBuilder sb;
     public static void main(String[] args)throws IOException{
         BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
-        int N=Integer.parseInt(bf.readLine());
-        StringBuilder sb=new StringBuilder();
+        String str;
 
-        if(N==0){
-            System.out.println("-");
-        }else if(N==1){
-            String m="- - ";
-        }else{
-            for(int i=0; i<N; i++){
-                sb.append(m);
-            }
+        while((str=bf.readLine())!=null && !str.isEmpty()){
+            int N=Integer.parseInt(str);
+
+            sb=new StringBuilder();
+
+            centor(N);
+
+            System.out.println(sb.toString());
         }
+
+
+        }
+
+
+    public static void centor(int N){
+        if(N==0){
+            sb.append("-");
+            return;
+        }
+        centor(N-1);
+
+        int len=(int)Math.pow(3,N-1);
+        for(int i=0; i<len; i++){
+            sb.append(" ");
+        }
+        centor(N-1);
     }
 }
