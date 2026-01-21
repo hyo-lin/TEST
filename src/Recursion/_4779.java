191aa52d7c7492a84bdb35a45e96557d0fd7da8e
hyo-lin <lin1137001@gmail.com>
2026-01-21 22:59:32 +0900

    4779

--- src/Recursion/_4779.java
+++ src/Recursion/_4779.java
@@ -9,32 +9,26 @@ public class _4779 {
     public static void main(String[] args)throws IOException{
         BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
         String str;
-
         while((str=bf.readLine())!=null && !str.isEmpty()){
-            int N=Integer.parseInt(str);
-
+            int N = Integer.parseInt(str);
             sb=new StringBuilder();
-
-            centor(N);
-
-            System.out.println(sb.toString());
+            solve(N);
+            System.out.println(sb);
         }
 
+    }
 
-        }
-
-
-    public static void centor(int N){
+    public static void solve(int N){
         if(N==0){
             sb.append("-");
             return;
         }
-        centor(N-1);
+        solve(N-1);
 
-        int len=(int)Math.pow(3,N-1);
-        for(int i=0; i<len; i++){
+        for(int i=0; i<Math.pow(3,N-1); i++){
             sb.append(" ");
         }
-        centor(N-1);
+        solve(N-1);
+
     }
 }
