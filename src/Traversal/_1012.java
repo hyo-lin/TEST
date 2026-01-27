01502dd4a98af52f3e6b05b2cfddf440e1478ba0
hyo-lin <lin1137001@gmail.com>
2026-01-27 23:36:56 +0900

    1012

--- src/Traversal/_1012.java
+++ src/Traversal/_1012.java
@@ -3,35 +3,74 @@ package Traversal;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class _1012 {
 
-    public static void main(String[] args)throws IOException{
-        BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+    static int[][] gp;
+    static boolean[][] bool;
 
-        int T=Integer.parseInt(bf.readLine());
-        StringTokenizer st=new StringTokenizer(bf.readLine());
+    static int T;
+    static int M;
+    static int N;
+    static int K;
 
-        for(int j=0; j<T; j++){
-            st=new StringTokenizer(bf.readLine());
-            int M=Integer.parseInt(st.nextToken());
-            int N=Integer.parseInt(st.nextToken());
-            int K=Integer.parseInt(st.nextToken());
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {1, -1, 0, 0};
 
-            for(int i=0; i<K; i++){
 
-                for(int l=0; l<T; l++){
-                    int u=Integer.parseInt(st.nextToken());
-                    int v=Integer.parseInt(st.nextToken());
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        T = Integer.parseInt(bf.readLine());
 
-                }
+
+
+
+        for (int j = 0; j < T; j++) {
+            StringTokenizer st = new StringTokenizer(bf.readLine());
+            M = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
+
+            gp = new int[N][M];
+            bool = new boolean[N][M];
+
+            for (int i = 0; i < K; i++) {
+                st = new StringTokenizer(bf.readLine());
+                int u = Integer.parseInt(st.nextToken());
+                int v = Integer.parseInt(st.nextToken());
+                gp[v][u] = 1;
 
             }
+
+            int count = 0;
+            for (int i = 0; i < N; i++) {
+                for (int l = 0; l < M; l++) {
+                    if (gp[i][l] == 1 && !bool[i][l]) {
+                        dfs(i, l);
+                        count++;
+                    }
+                }
+            }
+            System.out.println(count);
         }
+    }
 
 
+    public static void dfs(int y, int x) {
+        bool[y][x] = true;
 
+        for (int i = 0; i < 4; i++) {
+            int xn = dx[i] + x;
+            int yn = dy[i] + y;
+            if (xn >= 0 && yn >= 0 && xn < M && yn < N) {
+                if (gp[yn][xn] == 1 && !bool[yn][xn]) {
+                    dfs(yn, xn);
+                }
+            }
 
+        }
     }
 }
+
