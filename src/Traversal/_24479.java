0b8b004124737a5fcca3795002809af8b53ac2cd
hyo-lin <lin1137001@gmail.com>
2026-02-03 23:58:18 +0900

    24479

--- src/Traversal/_24479.java
+++ src/Traversal/_24479.java
@@ -9,53 +9,51 @@ import java.util.StringTokenizer;
 
 public class _24479 {
     static ArrayList<Integer>[] gr;
-    static int[] arr;
+    static int[] visited;
     static int count=1;
 
-    public static void main(String[] args)throws IOException{
-        BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st=new StringTokenizer(bf.readLine());
-        int N=Integer.parseInt(st.nextToken());
-        int M=Integer.parseInt(st.nextToken());
-        int R=Integer.parseInt(st.nextToken());
+    public static void main(String[] args)throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
 
         gr=new ArrayList[N+1];
-        arr=new int[N+1];
+        visited = new int[N + 1];
 
         for(int i=1; i<=N; i++){
             gr[i]=new ArrayList<>();
         }
 
-        for(int i=1; i<=M; i++){
+        for(int i=0; i<M; i++){
             st=new StringTokenizer(bf.readLine());
             int u=Integer.parseInt(st.nextToken());
             int v=Integer.parseInt(st.nextToken());
-
             gr[u].add(v);
             gr[v].add(u);
+
         }
 
         for(int i=1; i<=N; i++){
             Collections.sort(gr[i]);
         }
 
-
         dfs(R);
 
         StringBuilder sb=new StringBuilder();
         for(int i=1; i<=N; i++){
-            sb.append(arr[i]).append('\n');
+            sb.append(visited[i]).append("\n");
         }
         System.out.println(sb);
     }
 
-    public static void dfs(int node){
-        arr[node]=count;
+    static void dfs(int node){
+        visited[node]=count;
         count++;
 
         for(int next: gr[node]){
-
-            if(arr[next]==0){
+            if(visited[next]==0){
                 dfs(next);
             }
         }
