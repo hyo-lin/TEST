65157871057f370bf602851ddeff777a9ae0342f
hyo-lin <lin1137001@gmail.com>
2026-02-05 23:57:32 +0900

    7562

---
+++ src/Traversal/_7562.java
@@ -0,0 +1,12 @@
+package Traversal;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _7562 {
+    public static void main(String[] args)throws IOException{
+        BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+        int N=Integer.parseInt(bf.readLine());
+    }
+}
