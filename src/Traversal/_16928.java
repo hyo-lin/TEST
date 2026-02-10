ffe3b1d4985c276df5268cffb9e64446136af0a3
hyo-lin <lin1137001@gmail.com>
2026-02-10 23:55:29 +0900

    16928

--- src/Traversal/_16928.java
+++ src/Traversal/_16928.java
@@ -3,11 +3,16 @@ package Traversal;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class _16928 {
 
-    static int[][] arr;
+
+    static int[] board=new int[101];
+    static int[] count=new int[101];
+    static boolean[] bol=new boolean[101];
 
     public static void main(String[] args)throws IOException {
 
@@ -16,10 +21,51 @@ public class _16928 {
         int N=Integer.parseInt(st.nextToken());
         int M=Integer.parseInt(st.nextToken());
 
+        for(int i=1; i<=100; i++){
+            board[i]=i;
+        }
+
         for(int i=0; i<N; i++){
-            for(int j=0; j<2; j++){
-                arr[i][j]=Integer.parseInt(st.nextToken());
+            st=new StringTokenizer(bf.readLine());
+            int start=Integer.parseInt(st.nextToken());
+            int end=Integer.parseInt(st.nextToken());
+            board[start]=end;
+        }
+        for(int i=0; i<M; i++){
+            st=new StringTokenizer(bf.readLine());
+            int start=Integer.parseInt(st.nextToken());
+            int end=Integer.parseInt(st.nextToken());
+            board[start]=end;
+        }
+
+        bfs();
+    }
+
+    static void bfs(){
+        Queue<Integer> que=new LinkedList<>();
+        que.offer(1);
+        bol[1]=true;
+        count[1]=0;
+
+        while(!que.isEmpty()){
+            int curr=que.poll();
+
+            if(curr==100){
+                System.out.println(count[100]);
+                return;
+            }
+            for(int i=1; i<=6; i++){
+                int next=curr+i;
+                if(next>100) continue;
+
+                int target=board[next];
+                if(!bol[target]){
+                    bol[target]=true;
+                    count[target]=count[curr]+1;
+                    que.offer(target);
+                }
             }
         }
+
     }
 }
