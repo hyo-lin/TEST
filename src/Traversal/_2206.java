02df5a43c82773628adc06cf020d1f925ecefc82
hyo-lin <lin1137001@gmail.com>
2026-02-12 23:29:12 +0900

    2206

--- src/Traversal/_2206.java
+++ src/Traversal/_2206.java
@@ -9,69 +9,65 @@ import java.util.StringTokenizer;
 
 public class _2206 {
 
-    static Queue<int[]> que=new LinkedList<>();
+  static Queue<int[]> que=new LinkedList<>();
+  static int[] dx={0,0,-1,1};
+  static int[] dy={1,-1,0,0};
 
-    static int N;
-    static int M;
-    static int[] dx={0,0,-1,1};
-    static int[] dy={1,-1,0,0};
-    static int[][] board;
-    static boolean[][][] visited;
+  static int N;
+  static int M;
+  static int[][] board;
+  static boolean[][][] visited;
 
+  public static void main(String[] args)throws IOException{
+    BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st=new StringTokenizer(bf.readLine());
+    N=Integer.parseInt(st.nextToken());
+    M=Integer.parseInt(st.nextToken());
 
-    public static void main(String[] args)throws IOException{
-        BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st=new StringTokenizer(bf.readLine());
+    board=new int[N][M];
+    visited=new boolean[N][M][2];
 
-        N=Integer.parseInt(st.nextToken());
-        M=Integer.parseInt(st.nextToken());
-
-        board=new int[N][M];
-        visited = new boolean[N][M][2];
-
-        for(int i=0; i<N; i++){
-            String s=bf.readLine();
-            for(int j=0; j<M; j++){
-                board[i][j] = Integer.valueOf(s.charAt(j) - '0');
-            }
-        }
-
-        System.out.println(bfs());
+    for(int i=0; i<N; i++){
+      String s=bf.readLine();
+      for(int j=0; j<M; j++){
+        board[i][j]=s.charAt(j)-'0';
+      }
     }
 
-    static int bfs(){
+    System.out.println(bfs());
+  }
 
-        que.offer(new int[]{0,0,0,1});
-        visited[0][0][0] = true;
-        while(!que.isEmpty()){
-            int[] cur=que.poll();
-            int x = cur[0];
-            int y = cur[1];
-            int broken = cur[2];
-            int dist = cur[3];
+  static int bfs(){
+    que.offer(new int[]{0,0,0,1});
+    visited[0][0][0] = true;
+    while(!que.isEmpty()){
+      int[] m=que.poll();
+      int x=m[0];
+      int y=m[1];
+      int vi=m[2];
+      int dist=m[3];
 
-            if(x == N-1 && y == M-1){
-                return dist;
-            }
+      if(x==N-1 && y==M-1){
+        return dist;
+      }
 
-            for(int i=0; i<4; i++){
-                int nx = x + dx[i];
-                int ny = y + dy[i];
+      for(int i=0; i<4; i++){
+        int px=x+dx[i];
+        int py=y+dy[i];
 
-                if(nx>=0 && ny>=0 && nx<N && ny<M){
-                    if(board[nx][ny] == 0 && !visited[nx][ny][broken]){
-                        visited[nx][ny][broken] = true;
-                        que.offer(new int[]{nx, ny, broken, dist+1});
-                    }
-
-                    if(board[nx][ny] == 1 && broken == 0 && !visited[nx][ny][1]){
-                        visited[nx][ny][1] = true;
-                        que.offer(new int[]{nx,ny,1,dist+1});
-                    }
-                }
-
-            }
+        if(px>=0 && py>=0 && px<N && py<M){
+          if(board[px][py]==0 && !visited[px][py][vi]){
+            visited[px][py][vi]=true;
+            que.offer(new int[]{px,py,vi,dist+1});
+          }
+          // 아직 벽을 안 부쉈고 다음칸에 벽을 부수고 방문한적이 없음
+          if(board[px][py]==1 &&  vi == 0 && !visited[px][py][1]){
+            visited[px][py][1]=true;
+            que.offer(new int[]{px,py,1,dist+1});
+          }
         }
-        return -1;
+      }
     }
+    return -1;
+  }
 }
