2a64730d93da61ce3e48b0cd8ed899961979558c
hyo-lin <lin1137001@gmail.com>
2026-02-19 22:43:58 +0900

    1912

---
+++ src/DP/_1904.java
@@ -0,0 +1,23 @@
+package DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _1904 {
+    static int N;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(bf.readLine());
+        arr = new int[10000001];
+        arr[1] = 1;
+        arr[2] = 2;
+
+        for (int i = 3; i <= N; i++) {
+            arr[i] = (arr[i - 1] + arr[i - 2]) % 15746;
+        }
+        System.out.println(arr[N]);
+    }
+}
